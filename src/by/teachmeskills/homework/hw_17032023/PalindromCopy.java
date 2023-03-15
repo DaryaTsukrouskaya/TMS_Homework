cb2f2d2898fb3869a84c22c95dcaa0c597fe4bc5
Darya Tsukrovskaya <darya.raikhert.31@mail.ru>
2023-03-15 14:59:30 +0300

    changes

---
+++ src/by/teachmeskills/homework/hw_17032023/PalindromCopy.java
@@ -0,0 +1,21 @@
+package by.teachmeskills.homework.hw_17032023;
+
+import java.io.*;
+
+public class PalindromCopy {
+    public static <exc> void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("C:/Users/Admin/Desktop/palindromfile.txt"));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:/Users/Admin/Desktop/newpalindromfile.txt"))) {
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                StringBuilder palindromCheck = new StringBuilder(str);
+                if (str.equalsIgnoreCase(palindromCheck.reverse().toString())) {
+                    bufferedWriter.write(str + "\n");
+                    bufferedWriter.flush();
+                }
+            }
+        } catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+    }
+}
